9eede837e0181be564091770ebaff0e4cd565705
malezjaa <poreba.kris@gmail.com>
2026-05-24 01:44:52 +0200

    build dashboard in build.rs

--- build.rs
+++ build.rs
@@ -1,6 +1,12 @@
 use std::io;
+use std::process::Command;
 
 fn main() -> io::Result<()> {
+  Command::new("pnpm")
+    .arg("vite build")
+    .current_dir("dashboard")
+    .status()?;
+
   #[cfg(all(windows, not(debug_assertions)))]
   {
     let mut res = winres::WindowsResource::new();
