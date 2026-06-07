58224673dcfc61ad663b005871a9de414362fdfe
malezjaa <poreba.kris@gmail.com>
2026-06-07 14:28:53 +0200

    remove the data table

--- dashboard/src/app/dashboard/dashboard.tsx
+++ dashboard/src/app/dashboard/dashboard.tsx
@@ -1,11 +1,9 @@
 import { AppSidebar } from "@/components/app-sidebar"
 import { RequestsChart } from "@/components/requests-chart.tsx"
-import { DataTable } from "@/components/data-table"
 import { SectionCards } from "@/components/section-cards"
 import { SiteHeader } from "@/components/site-header"
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 
-import data from "./data.json"
 import * as React from "react"
 import Devices from "@/components/devices/devices.tsx"
 
@@ -33,8 +31,6 @@ export function Dashboard() {
               <div className="px-4 lg:px-6">
                 <Devices />
               </div>
-
-              <DataTable data={data} />
             </div>
           </div>
         </div>
