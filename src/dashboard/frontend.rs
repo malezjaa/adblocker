93b31cbfed652db3b5b62c0ace44bfa4cf29fe42
malezjaa <poreba.kris@gmail.com>
2026-06-11 17:05:55 +0200

    make dashboard serving work for routers

--- src/dashboard/frontend.rs
+++ src/dashboard/frontend.rs
@@ -9,18 +9,38 @@ use reqwest::StatusCode;
 pub const DIST: Dir = include_dir!("$CARGO_MANIFEST_DIR/dashboard/dist");
 
 pub async fn serve_file(Path(path): Path<String>) -> Result<impl IntoResponse, AppError> {
-  let file = DIST.get_file(&path);
-
-  if let Some(file) = file {
-    let mut response = Response::new(String::from_utf8(file.contents().to_vec())?);
-    let ty = from_path(path);
-
-    if let Some(ty) = ty.first() {
-      response.headers_mut().insert(header::CONTENT_TYPE, ty.to_string().parse()?);
-    }
-
-    Ok(response)
+  let file = if let Some(file) = DIST.get_file(&path) {
+    file
+  } else if path.contains('.') {
+    return Err(AppError::with_code(
+      format!("asset {path} not found"),
+      StatusCode::NOT_FOUND,
+    ));
   } else {
-    Err(AppError::with_code(format!("file {path} not found"), StatusCode::NOT_FOUND))
+    DIST.get_file("index.html").ok_or_else(|| {
+      AppError::with_code(
+        "index.html not found",
+        StatusCode::INTERNAL_SERVER_ERROR,
+      )
+    })?
+  };
+
+  let contents = String::from_utf8(file.contents().to_vec())?;
+
+  let mut response = Response::new(contents);
+
+  let mime = if path.contains('.') {
+    from_path(&path).first()
+  } else {
+    from_path("index.html").first()
+  };
+
+  if let Some(mime) = mime {
+    response.headers_mut().insert(
+      header::CONTENT_TYPE,
+      mime.to_string().parse()?,
+    );
   }
+
+  Ok(response)
 }
